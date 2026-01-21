0294fc0a88d6a62d04a119d56e2f2bcfcb66ac36
Firzi <firzi22ti@mahasiswa.pcr.ac.id>
2026-01-21 22:44:57 +0700

    config db using env railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,19 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'work_completion'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
-db.connect((err) => {
-if (err) {
-console.log('koneksi database gagal:', err)
-} else {
-console.log('database terhubung')
-}
+db.connect(err => {
+  if (err) {
+    console.error('koneksi database gagal:', err)
+  } else {
+    console.log('database terhubung')
+  }
 })
 
 module.exports = db
