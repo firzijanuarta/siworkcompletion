b7090afcf9b854f4227c86a56f04bd0f37c67cf6
Firzi <firzi22ti@mahasiswa.pcr.ac.id>
2026-01-22 15:02:39 +0700

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,33 @@
 const express = require('express')
 const cors = require('cors')
-const db = require('./config/db')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+
 const reportRoutes = require('./routes/reports')
 const detailRoutes = require('./routes/reportDetails')
 const exportRoutes = require('./routes/exportReport')
 const authRoutes = require('./routes/auth')
-const path = require('path')
 
-// FRONTEND
-app.use(express.static(path.join(__dirname, '../frontend')))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'))
-})
-
-// BACKEND
+// ===== MIDDLEWARE =====
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// ===== API ROUTES =====
 app.use('/api', reportRoutes)
 app.use('/api', detailRoutes)
-app.use('/uploads', express.static('uploads'))
 app.use('/api', exportRoutes)
 app.use('/api', authRoutes)
+app.use('/uploads', express.static('uploads'))
 
+// ===== ROOT CHECK =====
 app.get('/', (req, res) => {
-res.send('server work completion berjalan')
+  res.json({
+    status: 'OK',
+    message: 'Backend SiWorkCompletion berjalan'
+  })
 })
 
 app.listen(PORT, () => {
-console.log(`server running di http://localhost:` + PORT)
+  console.log(`server running di port ${PORT}`)
 })
